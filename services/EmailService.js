9af44dc1854b70130813194da29490a09c43fead
Yassin Rahou <yassin.rahou@hva.nl>
2024-10-16 21:47:27 +0200

    SYNRGY-70 | updated EmailService
    Minor adjustment in the constructor. Service now uses sendgrids smtp server and is complete for the time being

--- services/EmailService.js
+++ services/EmailService.js
@@ -14,26 +14,14 @@ class EmailService {
     transporter;
 
     constructor() {
-        nodemailer.createTestAccount((err, account) => {
-            this.transporter = nodemailer.createTransport({
-                host: account.smtp.host,
-                port: account.smtp.port,
-                secure: account.smtp.secure,
-                auth: {
-                    user: account.user,
-                    pass: account.pass
-                }
-            });
+        this.transporter = nodemailer.createTransport({
+            host: 'smtp.sendgrid.net',
+            port: 587,
+            auth: {
+                user: 'apikey',
+                pass: process.env.SENDGRID_API_KEY
+            }
         });
-
-        // Voor productie: gebruik Gmail-service
-        // this.transporter = nodemailer.createTransport({
-        //     service: 'Gmail',
-        //     auth: {
-        //         user: process.env.EMAIL, // zorg ervoor dat deze variabelen correct zijn ingesteld
-        //         pass: process.env.EMAIL_PASSWORD
-        //     }
-        // });
     }
 
     /**
@@ -46,7 +34,7 @@ class EmailService {
      */
     async sendVerificationEmail(email, code) {
         if (!this.transporter) {
-            throw new Error('Transporter is not initialized. Call init() first.');
+            throw new Error('Transporter not initialized');
         }
 
         // Load the email template in the templates folder
@@ -56,18 +44,14 @@ class EmailService {
         const content = template.replace('{{verificationCode}}', code);
 
         const options = {
-            from: email,
+            from: "To-do List <" + process.env.EMAIL + ">",
             to: email,
             subject: 'Email Verification',
             html: content
         };
 
         try {
-            const info = await this.transporter.sendMail(options);
-            console.log('Email sent:', info.response);
-            console.log('Preview URL:', nodemailer.getTestMessageUrl(info));
-
-            return info.response;
+            return await this.transporter.sendMail(options);
         } catch (error) {
             console.error('Error sending email:', error);
             throw error;
@@ -84,7 +68,7 @@ class EmailService {
      */
     async sendPasswordResetEmail(email, link) {
         if (!this.transporter) {
-            throw new Error('Transporter is not initialized. Call init() first.');
+            throw new Error('Transporter not initialized');
         }
 
         const template = fs.readFileSync(path.join(__dirname, '../templates/password-reset-email.html'), 'utf-8');
@@ -92,18 +76,14 @@ class EmailService {
         const content = template.replace('{{resetLink}}', link);
 
         const options = {
-            from: email,
+            from: "To-do List <" + process.env.EMAIL + ">",
             to: email,
             subject: 'Password Reset',
             html: content
         };
 
         try {
-            const info = await this.transporter.sendMail(options);
-            console.log('Email sent:', info.response);
-            console.log('Preview URL:', nodemailer.getTestMessageUrl(info));
-
-            return info.response;
+            return await this.transporter.sendMail(options);
         } catch (error) {
             console.error('Error sending email:', error);
             throw error;
