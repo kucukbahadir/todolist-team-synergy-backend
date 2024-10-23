0021b123920369da9382f0218174c101e95b7be0
Sameer Hanif <302548743@student.rocmondriaan.nl>
2024-10-23 17:34:24 +0200

    Usermodel aangepast had niet juist de github artiekel gelezen

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
-const Userschema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    }
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  verificationCode: {
+    type: String,
+    required: false // Only used temporarily during login
+  },
+  tasks: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Task' 
+  }],
+  sharedLists: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'TaskList'
+  }]
 }, { timestamps: true });
 
-module.exports = mongoose.model('email', Userschema);
+module.exports = mongoose.model('User', userSchema);
